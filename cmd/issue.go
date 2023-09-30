4ec912427b0be1963b4541d9434b86b16a334149
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-09-30 18:10:21 +0900

    Implemene `targetedCommand`

--- cmd/issue.go
+++ cmd/issue.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,6 +12,7 @@ type GhSosukeIssueError struct {
 	msg string
 	err error
 }
+
 func (e *GhSosukeIssueError) Error() string {
 	return fmt.Sprintf("Error from `gh sosuke issue`: %s\n%s", e.msg, e.err.Error())
 }
@@ -22,31 +24,59 @@ func list() (string, error) {
 	args := os.Args[3:]
 
 	cmdStr := fmt.Sprintf("gh issue list %s | peco", strings.Join(args, " "))
-	fmt.Println(cmdStr)
 	cmd := exec.Command("bash", "-c", cmdStr)
 	out, err := cmd.Output()
 	if err != nil {
-		return "", &GhSosukeIssueError{ msg: "Failed to run `gh issue list`", err: err }
+		return "", &GhSosukeIssueError{msg: "Failed to run `gh issue list`", err: err}
 	}
 	outStr := string(out)
 	return strings.Fields(outStr)[0], nil
 }
 
+func targetedCommand() (string, error) {
+	if len(os.Args) == 3 {
+		return "", &GhSosukeIssueError{msg: "`gh sosuke issue` needs issue number"}
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputValue := scanner.Text()
+	if err := scanner.Err(); err != nil {
+		return "", &GhSosukeIssueError{msg: "Failed to read input", err: err}
+	}
+
+	cmdStr := fmt.Sprintf("gh issue %s %s", strings.Join(os.Args[3:], " "), inputValue)
+
+	cmd := exec.Command("bash", "-c", cmdStr)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", &GhSosukeIssueError{msg: "Failed to run `gh issue`", err: err}
+	}
+	outStr := string(out)
+	return outStr, nil
+}
+
 func Issue() error {
 	if len(os.Args) == 2 {
-		return &GhSosukeIssueError{ msg: fmt.Sprintf("gh sosuke issue doesn't support `%s`", os.Args[2]) }
+		return &GhSosukeIssueError{msg: fmt.Sprintf("`gh sosuke issue` require sub-command")}
 	}
 
 	switch os.Args[2] {
 	case "list":
 		out, err := list()
-		_ = out
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(os.Stdout, "%s", out)
+		return nil
+	case "close", "create", "delete", "develop", "edit", "lock", "pin", "reopen", "transfer", "unlock", "unpin", "view":
+		out, err := targetedCommand()
 		if err != nil {
 			return err
 		}
 		fmt.Fprintf(os.Stdout, "%s", out)
 		return nil
 	default:
-		return &GhSosukeIssueError{ msg: fmt.Sprintf("gh sosuke issue doesn't support `%s`", os.Args[2]) }
+		return &GhSosukeIssueError{msg: fmt.Sprintf("`gh sosuke issue` doesn't support `%s`", os.Args[2])}
 	}
 }
